6c3472513aab5da2144e48a58d776a42bc10cb73
appuemmy <us.emmy@gmail.com>
2017-10-04 22:53:18 -0700

    Change-Id: I31cfe512baa9ded1f361d057b98d36899aa82506 Signed-off-by: appuemmy <us.emmy@gmail.com>

---
+++ CucumberBDD_Git_project/src/test/java/CucumberBDD_Git_project/CucumberBDD_Git_project/FirstGitClass.java
@@ -0,0 +1,10 @@
+package CucumberBDD_Git_project.CucumberBDD_Git_project;
+
+public class FirstGitClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is your first code");
+
+	}
+
+}
