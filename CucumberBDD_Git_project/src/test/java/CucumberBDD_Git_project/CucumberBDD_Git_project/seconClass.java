265710a3f87bb33090665b005f03a7d7788b3aaf
appuemmy <us.emmy@gmail.com>
2017-10-05 00:19:49 -0700

    This is second file
    Change-Id: I537a5bc4f1b4cf32703e4e22ab1de5d69f07cfb9
    Signed-off-by: appuemmy <us.emmy@gmail.com>

---
+++ CucumberBDD_Git_project/src/test/java/CucumberBDD_Git_project/CucumberBDD_Git_project/seconClass.java
@@ -0,0 +1,11 @@
+package CucumberBDD_Git_project.CucumberBDD_Git_project;
+
+public class seconClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is your 2nd code");
+		System.out.println("This is success too");
+
+	}
+
+}
